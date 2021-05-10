f057abc62a2a492ca7f0bc81ab2b99b4b72a2063
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-10 16:32:48 +0430

    initail commit

--- rest_api_app/asgi.py
+++ rest_api_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rest_api_project project.
+ASGI config for rest_api_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api_app.settings')
 
 application = get_asgi_application()
--- rest_api_app/urls.py
+++ rest_api_app/urls.py
@@ -1,4 +1,4 @@
-"""rest_api_project URL Configuration
+"""rest_api_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
