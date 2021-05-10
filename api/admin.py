9f0d2fd55ad0dcb3243d34e3b3cda5aa0cb37919
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-10 17:12:52 +0430

    PostListView added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post , Vote
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Vote)
--- rest_api_app/urls.py
+++ rest_api_app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
+
 
 urlpatterns = [
+    path('',views.PostList.as_view()),
     path('admin/', admin.site.urls),
+    
 ]
