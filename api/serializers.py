7d963ca44c4cfa7a8f6611bf8849df8927dcbac6
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-12 14:08:29 +0430

    first commit

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Post , Vote
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'title','url','poster','created']
