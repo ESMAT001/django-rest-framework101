bb2608f624000f2005b4e30cbf53ea8e7af1ea8c
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-09 12:19:12 +0430

    an commit

--- api/script.py
+++ api/script.py
@@ -4,9 +4,25 @@ from time import sleep
 from random import randint
 for _ in range(int(sys.argv[1])):
     rand=randint(int(sys.argv[2]),int(sys.argv[3]))
-    with open('./temp.py','a') as f:
+    with open('./model.py','a') as f:
         for _ in range(rand):
-            f.write("print('Hello world')\n")
+            f.write(""" from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    # likes = models.ForeignKey(User,on_delete=models.CASCADE)
+    poster= models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ['created_at']
+
+class Like(models.Model):
+    liker = models.ForeignKey(User,on_delete=models.CASCADE)
+    post  = models.ForeignKey(Post,on_delete=models.CASCADE) """)
         f.write('""" --IM_ES_N """\n')
     system('git add .')
     system('git commit -m "an commit"')
